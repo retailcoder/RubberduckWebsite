efa9d05c1ef088416818f3d8ec9774a141c6bb43
Mathieu Guindon <retailcoder@gmail.com>
2022-01-12 23:49:59 -0500

    added images and item view+viewmodel

---
+++ RubberduckWebsite/Models/FeatureItemViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Rubberduck.Model.Entities;
+
+namespace RubberduckWebsite.Models
+{
+    public class FeatureItemViewModel
+    {
+        public FeatureItemViewModel(FeatureItem item)
+        {
+            DateUpdated = item.DateUpdated ?? item.DateInserted;
+            Name = item.Name;
+            Title = item.Title;
+            Description = item.Description;
+            IsDiscontinued = item.IsDiscontinued;
+            IsNew = item.IsNew;
+            IsHidden = item.IsHidden;
+            XmlDocTabName = item.XmlDocTabName;
+            XmlDocSummary = item.XmlDocSummary;
+            XmlDocRemarks = item.XmlDocRemarks;
+            XmlDocInfo = item.XmlDocInfo;
+            XmlDocMetadata = item.XmlDocMetadata;
+            XmlDocSourceObject = item.XmlDocSourceObject;
+        }
+
+        public DateTime DateUpdated { get; }
+        public string Name { get; }
+        public string Title { get; }
+        public string Description { get; }
+
+        public bool IsDiscontinued { get; }
+        public bool IsNew { get; }
+        public bool IsHidden { get; }
+
+        public string XmlDocTabName { get; }
+        public string XmlDocSummary { get; }
+        public string XmlDocInfo { get; }
+        public string XmlDocRemarks { get; }
+        public string XmlDocMetadata { get; }
+        public string XmlDocSourceObject { get; }
+    }
+}
